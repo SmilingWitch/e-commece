5afc5bdd4b0777d3958d617078b7c82888607217
SmilingWitch <danielitayero@gmail.com>
2023-11-27 17:10:26 -0500

    feat: add Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+import style from "../../public/assets/styles/Contact.module.css"
+
+export default function Contact() {
+
+    return(
+        <div className={style.contact}>
+
+
+        </div>
+    )
+}
