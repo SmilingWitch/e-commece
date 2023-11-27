fe419bca8ffe8d2a0e7ef18b8560f3379f299e35
SmilingWitch <danielitayero@gmail.com>
2023-11-27 17:07:08 -0500

    feat: add page.js

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+"use-client"
+
+import Image from 'next/image'
+import styles from './page.module.css'
+import Home from '@/components/Home'
+
+export default function PrincipalPage() {
+  return (
+    <Home/>
+  )
+}
