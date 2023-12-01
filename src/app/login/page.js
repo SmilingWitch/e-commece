85c60c63860f53c2fa22f53255e86a03bb41753f
SmilingWitch <danielitayero@gmail.com>
2023-12-01 10:15:53 -0500

    feat: add login/

---
+++ src/app/login/page.js
@@ -0,0 +1,15 @@
+"use-client"
+
+import Image from 'next/image'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+import Navbar from '@/components/Navbar'
+
+export default function Index() {
+  return (
+    <>
+    <Login/>
+    </>
+    
+  )
+}
