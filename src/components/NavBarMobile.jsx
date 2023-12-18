4f043b50be30d7890fbfe91cc4c565405dd6655b
SmilingWitch <danielitayero@gmail.com>
2023-12-18 11:57:10 -0500

    feat: add NavBarMobile component

---
+++ src/components/NavBarMobile.jsx
@@ -0,0 +1,40 @@
+"use client"
+
+import style from "../../public/assets/styles/Navbar.module.css"
+import { MdSearch } from "react-icons/md";
+import { LuShoppingCart } from "react-icons/lu";
+import { FaRegUserCircle } from "react-icons/fa";
+import { SlWallet } from "react-icons/sl";
+import Link from "next/link"
+
+export default function NavBarMobile(){
+    return(
+        <div className={style.contMobile}>
+            <section className={style.superiorMobile}>
+            <Link href = "/home">
+                <div className={style.logo}>
+                    <img src="/assets/images/[removal.ai]_597ed435-d169-410c-962e-7dbf022aae9f-photo1702144866.png" alt="" />
+                    <span>rca Store</span>
+                </div>
+            </Link>
+                <div className={style.iconBx}>
+                    <div className={style.contIcon1}>
+                        <span className={style.icon1}><Link href = "/wallet"><SlWallet/></Link></span>
+                        <div className={style.cont1}>Billetera</div>
+                    </div>
+                    <div className={style.contIcon}>
+                        <span><LuShoppingCart/></span>
+                    </div>
+                    <div className={style.contIcon}>
+                        <span><MdSearch/></span>
+                    </div>
+                    <div className={style.contIcon1}>
+                        <span className={style.icon2}><Link href = "/accounts/login"><FaRegUserCircle/></Link></span>
+                        <div className={style.cont2}>Entrar o registrarse</div>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    )
+}
