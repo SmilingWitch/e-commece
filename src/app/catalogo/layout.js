b6b299ec904796a0f0fa905cf1f6a49fe4ce8c1f
SmilingWitch <danielitayero@gmail.com>
2023-12-20 21:57:05 -0500

    feat: add catalogo/layout

---
+++ src/app/catalogo/layout.js
@@ -0,0 +1,22 @@
+"use client"
+import MenuFloatinButton from '@/components/MenuFloatingBtn';
+import useMediaQuery from '../../components/functions/MediaQuery';
+import Navbar from '@/components/Navbar';
+import NavBarMobile from '@/components/NavBarMobile';
+import {MdSearch} from "react-icons/md"
+
+
+export default function RootLayout({ children }) {
+ const isMobile = useMediaQuery('(max-width: 768px)');
+
+ return (
+   <>
+     {isMobile ? <NavBarMobile /> : <Navbar />}
+     <MenuFloatinButton/>
+     <main >{children}</main>
+
+     
+    
+   </>
+ );
+}
