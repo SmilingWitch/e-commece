d3f740cb40f4cee030a73a3d0a3924ec9ea643e5
SmilingWitch <danielitayero@gmail.com>
2023-12-30 18:11:49 -0500

    feat: add my_account/

---
+++ src/app/my_account/page.js
@@ -0,0 +1,10 @@
+"use-client"
+import MyAccount from '@/components/MyAccount'
+import styles from '../page.module.css'
+import Home from '@/components/Home'
+
+export default function PrincipalPage() {
+  return (
+    <MyAccount/>
+  )
+}
