4214642bcd9a4d9e483f0f8928a1b9e7fed26b40
SmilingWitch <danielitayero@gmail.com>
2024-01-08 17:10:10 -0500

    feat: add links to SideBar component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -13,7 +13,7 @@ export default function SideBar(){
     return (
         <div className={style.cont}>
             <button>
-                <Link  href="dashboard/wallet" className={style.link}>
+                <Link  href="/dashboard/wallet" className={style.link}>
                     <span className={style.btnIcon}><HiOutlineHome/></span>
                     <span>Vista Principal</span>    
                 </Link> 
@@ -22,28 +22,28 @@ export default function SideBar(){
             <div className={style.finanzas}>
                 <h3>Finanzas</h3>
                 <ul>
-                    <li><Link href="dashboard/recibir"  className={style.link}>
+                    <li><Link href="/dashboard/recibir"  className={style.link}>
                         <span className={style.icon}><LuArrowDownToLine/></span>
                         <span>Recibir</span>
                         </Link>
                         
                     </li>
                     <li>
-                        <Link  href="dashboard/enviar"  className={style.link}>
+                        <Link  href="/dashboard/enviar"  className={style.link}>
                             <span className={style.icon}>< LuArrowUpFromLine/></span>
                             <span>Enviar</span>    
                         </Link> 
                        
                     </li>
                     <li>
-                        <Link  href="dashboard/guardar"  className={style.link}>
+                        <Link  href="/dashboard/guardar"  className={style.link}>
                             <span className={style.icon}><GrSave/></span>
                             <span>Guardar</span>
                         </Link> 
                         
                     </li>
                     <li>
-                        <Link  href="dashboard/donar"  className={style.link}>
+                        <Link  href="/dashboard/donar"  className={style.link}>
                             <span className={style.icon}><BiDonateHeart/></span>
                             <span>Donar</span>    
                         </Link> 
@@ -55,7 +55,7 @@ export default function SideBar(){
                 <h3>Recompensas</h3>
                 <ul>
                     <li>
-                        <Link  href="dashboard/recompensa"  className={style.link}>
+                        <Link  href="/dashboard/recompensa"  className={style.link}>
                             <span className={style.icon}><IoGiftOutline /></span>
                             <span>Cangear Codigo</span>    
                         </Link> 
@@ -68,7 +68,7 @@ export default function SideBar(){
                 <h3>Configuracion</h3>
                 <ul>
                     <li>
-                        <Link  href="dashboard/wallet"  className={style.link}>
+                        <Link  href="/dashboard/link"  className={style.link}>
                             <span className={style.icon}><LuLink/></span>
                             <span>Links de pago</span>    
                         </Link> 
@@ -80,7 +80,7 @@ export default function SideBar(){
                 <h3>Juegos</h3>
                 <ul>
                     <li>
-                        <Link  href="dashboard/"  className={style.link}>
+                        <Link  href="/dashboard/wallet"  className={style.link}>
                             <span className={style.icon}><IoGameControllerOutline/></span>
                             <span>Cerito</span>    
                         </Link> 
