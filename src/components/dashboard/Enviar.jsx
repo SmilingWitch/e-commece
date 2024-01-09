097a34d5231a395a6253d2f51b8ac2e47a1714bc
SmilingWitch <danielitayero@gmail.com>
2024-01-09 17:48:16 -0500

    feat: add contento to Enviar component

--- src/components/dashboard/Enviar.jsx
+++ src/components/dashboard/Enviar.jsx
@@ -1,6 +1,44 @@
+import style from "../../../public/assets/styles/Enviar.module.css"
+import { BsQrCodeScan } from "react-icons/bs";
+import Image from "next/image"
+
 
 export default function Enviar(){
     return(
-        <h1>Enviar</h1>
+        <div className={style.cont}>
+            <div className={style.content}>
+                <div className={style.header}>
+                  <div className={style.line}></div>
+                  <h3>Enviar OSP</h3> 
+                </div>
+
+                <div className={style.bx}>
+                    <div className={style.input}>
+                        <label >Introducir Codigo:</label>
+                        <div className={style.inputBx}>
+                            <input type="text" name="" id="" />
+                            <span><BsQrCodeScan/></span>
+                        </div>
+                       
+                    </div>
+                    <button>Verificar</button>
+                    <div>
+                        <div className={style.imageBx}>
+                            <Image src = "/assets/images/undraw_transfer_money_re_6o1h.svg"
+                            layout="fill"
+                            objectFit="contain" // Ajusta la imagen para cubrir todo el contenedor
+                            objectPosition="center"></Image>
+                        </div>
+
+                    </div>
+                    
+                    
+
+                </div>
+
+            </div>
+
+        </div>
+        
     )
 }
