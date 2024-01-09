b93b8129aa3f4fdefa1b555b7cf4858d069ba40f
SmilingWitch <danielitayero@gmail.com>
2024-01-09 17:49:01 -0500

    feat: add contento to Recompensa component

--- src/components/dashboard/Recompensa.jsx
+++ src/components/dashboard/Recompensa.jsx
@@ -1,6 +1,31 @@
+import style from "../../../public/assets/styles/Recompensa.module.css"
+import Image from "next/image"
 
 export default function Recompensa(){
     return(
-        <h1>Recompensa</h1>
+        <div className={style.cont}>
+            <div className={style.content}>
+                <div className={style.header}>
+                  <div className={style.line}></div>
+                  <h3>Cangear Codigo</h3> 
+                </div>
+                <div className={style.bx}>
+                    <div className={style.input}>
+                        <label >Cangear:</label>
+                        <input type="text" name="" id="" />
+                    </div>
+                    <button>Recibir Premio</button>
+                    <div className={style.regalo}>
+                        <Image src = "/assets/images/undraw_happy_birthday_re_c16u.svg"
+                        width = {300}
+                        height={300}></Image>
+                    </div>
+
+                </div>
+                
+
+            </div>
+
+        </div>
     )
 }
