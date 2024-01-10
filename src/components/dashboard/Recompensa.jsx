b99996ddd526c8d4bf3ba892f581423d737f3a73
SmilingWitch <danielitayero@gmail.com>
2024-01-10 16:39:16 -0500

    feat: add changes for unlogged user to Recompensa  component

--- src/components/dashboard/Recompensa.jsx
+++ src/components/dashboard/Recompensa.jsx
@@ -1,9 +1,18 @@
+"use client"
+
 import style from "../../../public/assets/styles/Recompensa.module.css"
 import Image from "next/image"
+import Link from "next/link"
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+
 
 export default function Recompensa(){
+
+    const { user } = useContext(AuthContext);
+
     return(
-        <div className={style.cont}>
+        user !== null ? <div className={style.cont}>
             <div className={style.content}>
                 <div className={style.header}>
                   <div className={style.line}></div>
@@ -20,12 +29,15 @@ export default function Recompensa(){
                         width = {300}
                         height={300}></Image>
                     </div>
-
                 </div>
-                
-
             </div>
-
+        </div> :
+        <div className={style.cont1}>
+          <Image src = "/assets/images/undraw_login_re_4vu2.svg" width = {300} height={300}></Image>
+          <div className={style.subHeader}>
+            Debe autenticarse primero
+          </div>
+          <Link href = "/accounts/login"><button>Autenticarse</button></Link>
         </div>
     )
 }
