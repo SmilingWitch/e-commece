9a75b706b6b5fa8884b19f9b3bcef644ac0b9681
SmilingWitch <danielitayero@gmail.com>
2024-01-15 21:57:24 -0500

    ref: delete details of Recibir

--- src/components/dashboard/Recibir.jsx
+++ src/components/dashboard/Recibir.jsx
@@ -1,5 +1,5 @@
 "use client"
-
+import {useRouter} from "next/navigation"
 import style from "../../../public/assets/styles/Recibir.module.css"
 import { BsQrCodeScan } from "react-icons/bs";
 import { useState, useEffect } from "react";
@@ -13,40 +13,85 @@ import axios from "axios"
 import { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import Link from "next/link"
+import { grey } from "@mui/material/colors";
+import Details from "./Details";
 
 export default function Recibir(){
+        const router = useRouter();
         const { user } = useContext(AuthContext);
         const [link, SetLink] = useState(false)
-        useEffect(() => {
-            AOS.init({
-              duration:1000
-          });
-          }, []);
+        const [res, SetRes] = useState([])
+        const [copyLink, setCopyLink] = useState("");
+        const [visible, SetVisible] = useState(false)
+        let code = [];
+        if (typeof window !== 'undefined') {
+         code = JSON.parse(localStorage.getItem('codigoPago')) || [];
+        }
+        const [codigo, setCodigo] = useState(code);
 
-          const [copyLink, setCopyLink] = useState("UYGbiyui67532e61532");
+
+
+
+        const [formValue,setFormValue]=useState({
+            amount:''
+          });
+          const handleChange= (event) => {
+            setFormValue({
+              ...formValue,
+              [event.target.name]:event.target.value
+            })
+          }
+        
 
           const createLink = async () =>{
                 const token = localStorage.getItem('access')
                 console.log(token)
                 console.log("Peticion")
                 try {
-                    const response = await axios.post('https://zona0.onrender.com/transfer/create-receive/', {amount: 30}, { 
+                    const response = await axios.post('https://zona0.onrender.com/transfer/create-receive/', {amount: formValue.amount}, { 
                         headers: {
                             'Authorization': 'Bearer ' + token
                         }
                       });
-                      
 
+                      // Guardar la nueva información del código en el estado
+                        setCodigo(response.data);
+
+                        // Recuperar los datos existentes del localStorage
+                        let codigos = JSON.parse(localStorage.getItem('codigoPago')) || [];
+
+                        // Agregar el nuevo código al arreglo
+                        codigos.push(response.data);
+
+                        // Almacenar el arreglo actualizado en el localStorage
+                        localStorage.setItem('codigoPago', JSON.stringify(codigos));
+
+                      console.log("CODIGO",localStorage.getItem('codigoPago'))
+
+                    SetRes(response.data)
                     console.log(response);
                     SetLink(true);
+                    setCopyLink(res.code)
+                    SetVisible(true)
+                    
                    } catch(error) {
                     console.log(error.response);
                    }
                    
           }
 
+          /*if(link){
+            router.push(`/dashboard/recibir/${res.code}`)
+          }*/
+
+                
+
     return(
-        user !== null ? <div className={style.cont}>
+ <div className={style.cont}>
+          {visible && (<Details 
+                          SetVisible = {SetVisible}
+                          recibir = {res}
+                          />) }
         <div className={style.content}>
             <div className={style.header}>
               <div className={style.line}></div>
@@ -54,57 +99,34 @@ export default function Recibir(){
             </div>
 
             <div className={style.bx}>
-                <div className={style.input}>
-                    <label >Introducir Monto:</label>
-                    <div className={style.inputBx}>
-                        <input type="text" name="" id="" />
-                        <span>SOP</span>
-                    </div>
-                </div>
-                <button onClick = {createLink}>Generar Link</button>
 
                 <div className={style.description}>
                     <span>Description: </span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae possimus, non repellendus consequatur expedita dolorem dignissimos ullam hic cum illo quos reiciendis accusamus, ut voluptatibus libero, mollitia culpa. Placeat, nam?</p>
+                    <p>Introduzca la cantidad de SOP que desea recibir para generar un codigo de pago.</p>
                 </div>
-                    
-                {link && <div className={style.linkDetails} data-aos="zoom-in-up">
-                    <div className={style.imageBx} data-aos="zoom-in-up">
-                        <QRCode
-                             value="https://example.com"
-                             bgColor="#FFFFFF"
-                             fgColor="#000000"
-                             level="H"
-                             style={{ height: "auto", width: "100%" }}/> 
-                    </div>
-                    <div className={style.details} >
-                        <div className={style.detail}>
-                            <span className={style.name}>Codigo: </span>
-                            <span className={style.link}>
-                                <span>UYGbiyui67532e61532</span>
-                                <span className={style.icon} onClick={() => copyToClipboard(copyLink)}><IoMdCopy/></span>
-                            </span>
-                        </div>
-                        <div className={style.detail}>
-                            <span>Monto a pagar: </span>
-                            <span>1000.00 OSP</span>
-                        </div>
-                        <div className={style.detail}>
-                            <span>Estado: </span>
-                            <span>No efectuado</span>
-                        </div>
 
+                <div className={style.input}>
+                    <label >Monto a recibir:</label>
+                    <div className={style.inputBx}>
+                        <input  type="text" 
+                                name="amount" id=""
+                                value={formValue.amount}
+                                onChange={handleChange} 
+                                onKeyDown={(event) => {
+                                    const keyCode = event.keyCode;
+                                    const isNumber = (keyCode >= 48 && keyCode <= 57);
+                                    const isBackspace = (keyCode === 8);
+                                    if (!isNumber && !isBackspace) {
+                                      event.preventDefault();
+                                    }
+                                  }}/>
+                        <span>SOP</span>
                     </div>
-
-                </div>}
+                </div>
+                {formValue.amount === "" ? <button className={style.deseableBtn}>Generar Link</button> :
+                <button onClick = {createLink}>Generar Codigo</button>}
             </div>
         </div>
-    </div>: <div className={style.cont1}>
-          <Image src = "/assets/images/undraw_login_re_4vu2.svg" width = {300} height={300}></Image>
-          <div className={style.subHeader}>
-            Debe autenticarse primero
-          </div>
-          <Link href = "/accounts/login"><button>Autenticarse</button></Link>
-        </div>
+    </div>
     )
 }
