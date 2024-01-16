a6c3f3d93ddbb8cf67bc19413e42e37f4689116b
SmilingWitch <danielitayero@gmail.com>
2024-01-15 21:45:23 -0500

    feat: add [id]/

---
+++ src/app/dashboard/recibir/[id]/page.js
@@ -0,0 +1,10 @@
+"use-client"
+
+import Details from "@/components/dashboard/Details"
+import Recibir from "@/components/dashboard/Recibir"
+
+export default function Index() {
+  return (
+    <Details/>
+  )
+}
