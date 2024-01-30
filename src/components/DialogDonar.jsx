3324e28abfc17a20ab9852aef33d423df9e21a3f
SmilingWitch <danielitayero@gmail.com>
2024-01-30 15:43:26 -0500

    feat: add DialogDonar

---
+++ src/components/DialogDonar.jsx
@@ -0,0 +1,92 @@
+"use client"
+import style from "../../public/assets/styles/DialogDonar.module.css"
+import BeatLoader from "react-spinners/BeatLoader"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect, useState } from "react";
+import axios from "axios"
+
+export default function DialogDonar({SetActive, institution, user,SetAmount}){
+    const [loading, setLoading] = useState(false)
+    const [formValue,setFormValue]=useState({
+        amount:'',
+        user: user,
+        institution: institution
+      });
+
+      const handleChange= (event) => {
+        setFormValue({
+          ...formValue,
+          [event.target.name]:event.target.value
+        })
+      }
+
+
+
+    useEffect(() => {
+        AOS.init({
+          duration:200
+      });
+      }, []);
+
+
+
+
+      const donate = async () =>{
+        const token = sessionStorage.getItem('access')
+        console.log(token)
+        console.log(formValue)
+        setLoading(true)
+        try {
+            const response = await axios.post('https://zona0.onrender.com/institutions/donations/', formValue, { 
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                }
+              });
+              console.log(response.data)
+              SetAmount(formValue.amount)
+            setLoading(false)
+            SetActive(false)
+           } catch(error) {
+            console.log(error.response);
+            setLoading(false)
+           }
+           
+  }
+
+
+    return(
+        <div className={style.cont} onClick = {(event) => {event.stopPropagation()}}>
+            <div className={style.bx} data-aos="zoom-in">
+                <div className={style.header}>
+                   Monto a donar
+                </div>
+                <div className={style.content}>
+                    <input type="text"
+                    placeholder="0.00"
+                    name = "amount"
+                    value={formValue.amount}
+                    onChange={handleChange} />
+                </div>
+                {loading === false ? <div className={style.btnBx}>
+                    
+                    <button onClick = {() => SetActive(false)} className={style.cancel}>Cancelar</button>
+                    <button onClick = {donate}>Aceptar</button>
+                </div>: <div className={style.centerLoader}>
+                    <div className="sweet-loading">
+                          <BeatLoader
+                            color="rgba(255, 68, 0,1)"
+                            cssOverride={{}}
+                            margin={10}
+                            size={10}
+                            speedMultiplier={1}
+                          />
+                        </div>
+                </div>
+                }
+
+            </div>
+            
+        </div>
+    )
+}
