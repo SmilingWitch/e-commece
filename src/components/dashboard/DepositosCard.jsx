b6ccbbac20a1056bfed5154a82baf8f496d4698b
SmilingWitch <danielitayero@gmail.com>
2024-02-03 19:08:15 -0500

    feat: add new inputs to card in DepositosCard

--- src/components/dashboard/DepositosCard.jsx
+++ src/components/dashboard/DepositosCard.jsx
@@ -16,13 +16,16 @@ export default function DepositosCard({res,setVisible, setSelected}){
     return(
         <div className={style.card} >
             <div className={style.info}>
-                <div className={style.gain}><span>Ganancia mensual:</span> 3%</div>
-                <div className={style.amount}><span>Monto inicial:</span> {res.amount} OSP</div>
-                <div className={style.date}> {res.date}</div>
+                <div className={style.date}> <span>{res.date}</span></div>
+                <div className={style.gain}><span>Ganancia mensual</span> <span>3%</span></div>
+                <div className={style.amount}><span>Monto inicial</span> <span>{res.amount} OSP</span></div>
+                <div className={style.block}><span>Ganancia</span> <span>0.00 OSP</span></div>
+                <div className={style.block}><span>Tiempo de bloqueo</span> <span>{res.date_banked} dia(s)</span></div>
+                
             </div>
             <div className={style.btnBx}>
-                <div className={style.progreso}>
-                    <button>{/*Progreso*/} {res.state} </button>
+                {/*<div className={style.progreso}>
+                    <button> {res.state} </button>
                     <div className={style.popUp}>
                         <PiInfoLight className={style.icon}/>
                         <div className={style.hover}>
@@ -30,8 +33,8 @@ export default function DepositosCard({res,setVisible, setSelected}){
 
                         </div>
                         
-                    </div>
-                </div>
+    </div>
+                </div>*/}
                 
             <button onClick={() => {setVisible(true); setSelected(res.id);console.log(res.id)}}>Retirar</button>
             </div>
